b704bbc76f33522749b34fa6fdf59e12dbb3c0ae
ani721424 <anirudhadey549@gmail.com>
2021-12-17 00:35:43 +0530

    hey commit

--- news/settings.py
+++ news/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'news.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'Templates')],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
